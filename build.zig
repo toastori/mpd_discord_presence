83e64e0f77e9e3c69df99a380fb853634b9be656
toastori <valbian@gmail.com>
2025-11-10 18:38:53 +0800

    rename project (missing)

--- build.zig
+++ build.zig
@@ -13,7 +13,7 @@ pub fn build(b: *std.Build) void {
     });
 
     const exe = b.addExecutable(.{
-        .name = "mpd_drpc",
+        .name = "mpd_discord_presence",
         .root_module = exe_mod,
     });
 
